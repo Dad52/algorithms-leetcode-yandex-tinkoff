46eed6c085cdbdfa5b2cf6bc5b7bb02c40038a91
Sobolev Andrey <93911341+Dad52@users.noreply.github.com>
2023-01-30 12:15:05 +0300

    Add files via upload

---
+++ leetcode/665923933_roman-to-integer.kt
@@ -0,0 +1,38 @@
+w4DhLj7yr9Mu5Nz
+
+Придумай случай, когда это решение не пройдет
+csrtoken: 1pGNQ3ojp4eX4gJge84He8x0squ4eO0eiOOKydJS23kRXPkTBYfQIjJL2p5K0Fgb
+session: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9
+gr_user_id=14993dbe-c4ac-468f-8a39-da64b6c23810; __stripe_mid=35567d71-7767-49b1-b29e-83ac3acd376b401251; NEW_PROBLEMLIST_PAGE=1; __atuvc=0|42,0|43,5|44,1|45,3|46; c_a_u="RGFkNTI=:1ozi6w:hvVaB1RIq7T4Mzsu8xNqHwgvqZY"; __utmc=217727755; csrftoken=1pGNQ3ojp4eX4gJge84He8x0squ4eO0eiOOKydJS23kRXPkTBYfQIjJL2p5K0Fgb; 87b5a3c3f1a55520_gr_last_sent_cs1=Dad52; _gid=GA1.2.258494403.1674557655; LEETCODE_SESSION=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfYXV0aF91c2VyX2lkIjoiNjA0NDAzNCIsIl9hdXRoX3VzZXJfYmFja2VuZCI6ImFsbGF1dGguYWNjb3VudC5hdXRoX2JhY2tlbmRzLkF1dGhlbnRpY2F0aW9uQmFja2VuZCIsIl9hdXRoX3VzZXJfaGFzaCI6IjgwNGQ5MjdmOGE1NDI1MWFhMTFjZmRjY2YwYTkyMGU2NjQ0YzVlNDciLCJpZCI6NjA0NDAzNCwiZW1haWwiOiJhbmRyZXk5NTQ2NUBnbWFpbC5jb20iLCJ1c2VybmFtZSI6IkRhZDUyIiwidXNlcl9zbHVnIjoiRGFkNTIiLCJhdmF0YXIiOiJodHRwczovL2Fzc2V0cy5sZWV0Y29kZS5jb20vdXNlcnMvYXZhdGFycy9hdmF0YXJfMTY1MzA3NjEyMC5wbmciLCJyZWZyZXNoZWRfYXQiOjE2NzQ5MjI3MjYsImlwIjoiMjEzLjE5My43LjY1IiwiaWRlbnRpdHkiOiJhODE4YWIzNTk4MDQ1MTdmMjU0OWU5NGM4OGQwM2MwYiIsInNlc3Npb25faWQiOjMyNjg4NTAyfQ.EpRvw6275k5Msr7ZN2ov2YOmcv5MdVw9OUosymOq31I; _ga_CDRWKZTDEX=GS1.1.1675002637.187.1.1675006973.0.0.0; _dd_s=; _ga=GA1.2.2060517640.1650737100; _gat=1; __utma=217727755.2060517640.1650737100.1675002639.1675011035.59; __utmz=217727755.1675011035.59.7.utmcsr=github.com|utmccn=(referral)|utmcmd=referral|utmcct=/; __utmt=1; __utmb=217727755.1.10.1675011035; 87b5a3c3f1a55520_gr_session_id=f7a46e54-f8fc-4a53-9e3e-5d432a7be521; 87b5a3c3f1a55520_gr_last_sent_sid_with_cs1=f7a46e54-f8fc-4a53-9e3e-5d432a7be521; 87b5a3c3f1a55520_gr_cs1=Dad52; 87b5a3c3f1a55520_gr_session_id_f7a46e54-f8fc-4a53-9e3e-5d432a7be521=true
+
+
+
+
++1. Очереди - тип данных с доступом к элементам по принципу «первый пришёл — первый вышел»
++2. Декоратор
++3. branch
++4. merge
++5. Делегирование
++6. Инкапсуляция
++7. F
++8. logn
++9. Обращение к static полю осуществляется через имя класса
++10. Старта приложения
++11. everything
++12. В консоль будет выведено "Hello World"
++13. static метод может быть вызван из non-static метода.
++14. onetwoonethree
++15. RuntimeException
++16. JRE
++17. DELETE
++18. SELECT FirstName FROM Employee
++19. MAKE
++20. Все из вышеперечисленного
++21. в фоновом потоке
++22. Сколько угодно раз.
++23. Он исполняется на каждую подписку
++24. В Internal Storage в директории cache
+
+
+
+

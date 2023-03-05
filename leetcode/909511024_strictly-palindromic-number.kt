83d8db3544e1dc11e4b3cf4570d3171ef8d23dfc
Dad52 <Dad52@users.noreply.github.com>
2023-03-05 12:30:59 +0000

    Adding new LC solutions

---
+++ leetcode/909511024_strictly-palindromic-number.kt
@@ -0,0 +1,13 @@
+id = 909511024 
+lang = kotlin 
+runtime  = 139 ms 
+memory = 32.9 MB
+title_slug = strictly-palindromic-number
+code =
+
+ class Solution {
+    fun isStrictlyPalindromic(n: Int): Boolean {
+        return false
+    }
+
+}
